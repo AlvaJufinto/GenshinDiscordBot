232341b3938f0d60f53e3f54a5d136107c470898
unknown <yeahbruh199@gmail.com>
2022-07-12 21:23:15 +0700

    ah yes that's all it takes to generate 'hello there'

---
+++ src/Bot.ts
@@ -0,0 +1,19 @@
+import { Client, ClientOptions } from "discord.js";
+import interactionCreate from "./listeners/interactionCreate";
+import ready from "./listeners/ready";
+import "dotenv/config";
+
+const token: string = process.env.BOT_TOKEN as string;
+
+console.log("Bot is starting...");
+const client = new Client({
+    intents: []
+})
+
+ready(client);
+interactionCreate(client);
+
+client.login(token)
+
+// console.log(client);
+
---
+++ src/Command.ts
@@ -0,0 +1,6 @@
+import { BaseCommandInteraction, ChatInputApplicationCommandData, Client } from "discord.js";
+
+export interface Command extends ChatInputApplicationCommandData {
+    run: (client: Client, interaction: BaseCommandInteraction) => void;
+}
+
