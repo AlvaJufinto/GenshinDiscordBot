38b7ee6269a864e1c4100ab3835f1adc426dfb2f
unknown <yeahbruh199@gmail.com>
2022-07-13 22:06:55 +0700

    Trying the api

--- src/ApiCalls.ts
+++ src/ApiCalls.ts
@@ -1,3 +1,16 @@
-export function getElements(): void {
-    
-}
+const baseUrl: string = "https://api.genshin.dev/";
+
+export const publicRoute: object = {
+    getAllTypes: (): object => {
+        return fetch(baseUrl);
+    },
+    getElements: (): object => {
+      return fetch(`${baseUrl}/elements`)
+    },
+    // getPost: (body: string) => {
+    //   return fetch(`${baseUrl}/public/post/${body}`);
+    // },
+    // getPostLikes: (body: string) => {
+    //   return fetch(`${baseUrl}/public/post/likes/${body}`)
+    // }
+  }
--- src/commands/Hello.ts
+++ src/commands/Hello.ts
@@ -1,16 +1,35 @@
 import { BaseCommandInteraction, Client } from "discord.js";
 import { Command } from "../Command";
+import { publicRoute } from "./../ApiCalls";
 
 export const Hello: Command = {
     name: "hello",
     description: "Returns a greeting",
     type: "CHAT_INPUT",
-    run: async (client: Client, interaction: BaseCommandInteraction) => {
-        const content = "Hello there!";
-
-        await interaction.followUp({
-            ephemeral: true,
-            content
+    run: 
+        async (client: Client, interaction: BaseCommandInteraction) => {
+            const content = "Hello there!";
+            
+            await interaction.followUp({
+                ephemeral: true,
+                content
         });
     }
 }; 
+
+export const ElementList: Command = {
+    name: "get elements",
+    description: "Show elements list",
+    type: "CHAT_INPUT",
+    run: 
+        async (client: Client, interaction: BaseCommandInteraction) => {
+            // const res = await publicRoute.getElements(); 
+            // console.log(res);
+            const content = "Hello there!";
+            
+            await interaction.followUp({
+                ephemeral: true,
+                content
+        });
+    }
+}
