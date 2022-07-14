f643ce0574e49836a5cd724e03f689f7b587e394
unknown <yeahbruh199@gmail.com>
2022-07-14 16:09:41 +0700

    Api works

--- src/ApiCalls.ts
+++ src/ApiCalls.ts
@@ -1,23 +1,12 @@
 import fetch from 'node-fetch';
-const baseUrl: string = "https://api.genshin.dev/";
+const baseUrl: string = "https://api.genshin.dev";
 
-export const publicRoute: object = {
-    getAllTypes: async (): Promise<object> => {
-        try {
-            let res = await fetch(baseUrl);
-            return res; 
-        } catch (err) {
-            return err as object;
-        }
-
-    },
-    getElements: (): object => {
-        return fetch(`${baseUrl}/elements`)
-    },
-    // getPost: (body: string) => {
-    //   return fetch(`${baseUrl}/public/post/${body}`);
-    // },
-    // getPostLikes: (body: string) => {
-    //   return fetch(`${baseUrl}/public/post/likes/${body}`)
-    // }
-  }
+// export const publicRoute: object = {
+export const getAllTypes = async (): Promise<object> => {
+    try {
+        let res = await fetch(`${baseUrl}/elements`);
+        return await res.json(); 
+    } catch (err) {
+        return err as object;
+    }
+}
--- src/commands/Hello.ts
+++ src/commands/Hello.ts
@@ -1,6 +1,6 @@
 import { BaseCommandInteraction, Client } from "discord.js";
 import { Command } from "../Command";
-import { publicRoute } from "./../ApiCalls";
+import { getAllTypes } from "./../ApiCalls";
 
 export const Hello: Command = {
     name: "hello",
@@ -23,9 +23,9 @@ export const ElementList: Command = {
     type: "CHAT_INPUT",
     run: 
         async (client: Client, interaction: BaseCommandInteraction) => {
-            const res = await publicRoute.getAllTypes(); 
+            const res = await getAllTypes(); 
             console.log(res);
-            // const content = res;
+            const content = "yes";
             
             await interaction.followUp({
                 ephemeral: true,
